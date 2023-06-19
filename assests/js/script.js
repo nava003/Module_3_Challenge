e3e2ee5f9b7bf36872d8a52d08f6fb1c2163bf74
NavaHub <navaa003@gmail.com>
2023-06-18 21:07:33 -0500

    Checkpoint on .js codework; edited README extensively for easier readability

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+// Alert() them the possible choices in generating a new password
+alert("Here are the following options available:\n\
+-- Numeric Characters (0-9)\n\
+-- Alphabetic Characters (A-Z) (Upper and Lowercase)\n\
+-- Special Characters (!@#$/... etc.)");
+
+// Creating a function to begin asking for password criteria
+function generatePassword() {
+  
+  var numericSet = "0123456789";
+  var alphaUpperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var alphaLowerSet = "abcdefghijklmnopqrstuvwxyz";
+  var specialSet = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  // Confirm() them the first criteria question via IF Statement
+  if (confirm("Would you like your password to include Numeric Characters?\n\n\
+  Respond with Yes [OK], or No [Cancel]")) {
+
+    
+  }
+  else {
+    
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
